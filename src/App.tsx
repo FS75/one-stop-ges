ecdabde32a6b189f30aee0ae58242cfb16c13f84
Juleus <93301912+FS75@users.noreply.github.com>
2023-07-03 21:38:17 +0800

    Added table dark bg (for handling light mode)

--- src/App.tsx
+++ src/App.tsx
@@ -187,17 +187,17 @@ const App = () => {
                     {
                       sortedData.filter(row => parseInt(row.year) === currentTab).map((row, j) => (
                         <tr key={j}  className="border-b border-gray-800">
-                          <td className='text-center py-4'><Text>{j + 1}</Text></td>
-                          <td className='text-center py-4'><Text>{mapToAcronym(row.university)}</Text></td>
-                          <td className='py-4' style={{maxWidth: '300px'}}><Text>{row.degree}</Text></td>
-                          <td className='text-center py-4'><Text>{row.employment_rate_overall}</Text></td>
-                          <td className='text-center py-4'><Text>{row.employment_rate_ft_perm}</Text></td>
-                          <td className='text-center py-4'><Text>{row.basic_monthly_mean}</Text></td>
-                          <td className='text-center py-4'><Text>{row.basic_monthly_median}</Text></td>
-                          <td className='text-center py-4'><Text>{row.gross_monthly_mean}</Text></td>
-                          <td className='text-center py-4'><Text>{row.gross_monthly_median}</Text></td>
-                          <td className='text-center py-4'><Text>{row.gross_mthly_25_percentile}</Text></td>
-                          <td className='text-center py-4'><Text>{row.gross_mthly_75_percentile}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{j + 1}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{mapToAcronym(row.university)}</Text></td>
+                          <td className='bg-custom py-4' style={{maxWidth: '300px'}}><Text>{row.degree}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.employment_rate_overall}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.employment_rate_ft_perm}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.basic_monthly_mean}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.basic_monthly_median}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.gross_monthly_mean}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.gross_monthly_median}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.gross_mthly_25_percentile}</Text></td>
+                          <td className='text-center bg-custom py-4'><Text>{row.gross_mthly_75_percentile}</Text></td>
                         </tr>
                       ))
                     }
